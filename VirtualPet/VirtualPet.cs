25d98295e8b6dc6535b51709f89a3d823e420c1e
Brad Kilkenney <bkilkenney@gmail.com>
2016-09-18 06:22:40 -0400

    Properties, methods and comments

--- VirtualPet/VirtualPet.cs
+++ VirtualPet/VirtualPet.cs
@@ -8,60 +8,70 @@ namespace VirtualPet
 {
     class VirtualPet
     {
-        //Private access modifiers - these variables can only be accessed from within the class
+        //Private instance variables - these variables can only be accessed from within the class and can't be manipulated by outside forces
         private string name;
         private int apetite;
-        private int mood;
+        private int crabiness;
+        private int sleepiness;
 
         //Public access modifiers can be seen or used from anywhere, even outside the class.  CONSTRUCTORS
-        public VirtualPet(string name, int apetite, int mood)
-        {
-            this.name = name;
+        public VirtualPet(string name, int apetite, int crabiness, int sleepiness)
+        {            
             this.apetite = apetite;
-            this.mood = mood;
+            this.crabiness = crabiness;
+            this.sleepiness = sleepiness;
         }
 
         //Properties
 
         public string Name
+        {
+           get
             {
-            get { return this.name; }
-            set { this.name = value; }
-            }
+                return this.name;
+            } 
+             //Can have a get without a set.  Name won't change in this console app.  
+        }
 
         public int Apetite
         {
-            get { return this.apetite; }
+            get { return apetite; }  //This format is more concise but relies less on reading in a cascading pattern
             set { this.apetite = value; }
         }
         
         public int Mood
         {
-            get { return this.mood; }
-            set { this.mood = value; }
+            get { return crabiness; }
+            set { this.crabiness = value; }
+        }
+        public int Sleepiness
+        {
+            get { return sleepiness; }
+            set { this.sleepiness = value; }
         }
 
         //Methods
         public int FeedMe()
         {
-            apetite += 3;
-            mood += 3;
-            return apetite;
-                     
+            apetite -= 5;
+            crabiness += 3;
+            return apetite;  //Need a return to prevent errors               
         }
 
         public int Haunt()
         {
-            mood += 100;
+            crabiness += 15; //CrabAppleHead's fav activity is to haunt
             apetite -= 3;
-            return mood;
+            sleepiness += 5;
+            return crabiness;
         }
 
         public int Nap()
         {
             apetite += 5;
-            mood += 5;
-            return apetite;
+            crabiness += 5;
+            sleepiness -= 5;
+            return apetite; 
         }
 
      }
