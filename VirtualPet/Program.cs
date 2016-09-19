fe4d9e310f9cfc96f71a7dadad90663245bf407a
Brad Kilkenney <bkilkenney@gmail.com>
2016-09-18 21:57:05 -0400

    Added Timer

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -10,13 +10,23 @@ namespace VirtualPet
     {
         static void Main(string[] args)
         {
+            System.Threading.Thread.Sleep(1000);
+
             VirtualPet CrabAppleHead = new VirtualPet("CrabAppleHead", 25, 25, 25);
+            System.Threading.Thread.Sleep(1000);
             Console.WriteLine("Hello, I'm Crabapplehead, what would you like to do today?");
+            System.Threading.Thread.Sleep(1000);
             Console.WriteLine("Select from the following menu:");
+            System.Threading.Thread.Sleep(1000);
             Console.WriteLine();
+            System.Threading.Thread.Sleep(2000);
             Console.WriteLine("1: Eat Crabapples");
+            System.Threading.Thread.Sleep(2000);
             Console.WriteLine("2: Haunt Children");
+            System.Threading.Thread.Sleep(2000);
             Console.WriteLine("3: Nap");
+
+           
 
             int answer = int.Parse(Console.ReadLine());
 
@@ -37,10 +47,10 @@ namespace VirtualPet
             }
             else
             {
-                Console.WriteLine("Invalid answer, please select again");
+                Console.WriteLine("Invalid answer, Crabapplehead doesn't want to be you pet anymore, gave over.");
             }
 
-/*Need to make the game loop until player wins or loses, keep the current score on the screen,
+/*Need to make the game loop until player wins or loses,
 add in Tick and then try to implement stretch features*/
 
         }
